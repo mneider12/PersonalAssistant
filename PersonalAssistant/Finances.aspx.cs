1ba81242c3928747f5c7898bdc055e39fe15f1d7
Mark Nyder <mneider.virginia@gmail.com>
2017-01-08 21:51:50 -0600

    Add HtmlAgilityPack

--- PersonalAssistant/Finances.aspx.cs
+++ PersonalAssistant/Finances.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,6 +13,21 @@ namespace PersonalAssistant
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            WebRequest visaQuoteRequest = WebRequest.Create("http://finance.yahoo.com/quote/v");
+            using (WebResponse visaQuoteResponse = visaQuoteRequest.GetResponse())
+            {
+                using (Stream visaQuoteStream = visaQuoteResponse.GetResponseStream())
+                {
+                    using (StreamReader visaQuoteStreamReader = new StreamReader(visaQuoteStream))
+                    {
+                        string quoteLine;
+                        while ((quoteLine = visaQuoteStreamReader.ReadLine()) != null)
+                        {
+                            Console.WriteLine(quoteLine);
+                        }
+                    }
+                }
+            }
 
         }
     }
