b447a015438d6cd0c09ba0f03f5ea11fe4efbaea
Mark Nyder <mneider.virginia@gmail.com>
2017-01-14 14:59:29 -0600

    Create finances namespace and order.cs

---
+++ PersonalAssistant/Finances/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonalAssistant.Finances
+{
+    public class Order
+    {
+        private string ticker;
+        private double numShares;
+        private double price;
+        private DateTime date;
+        public Order(string ticker, double numShares, double price, DateTime date)
+        {
+            this.ticker = ticker;
+            this.numShares = numShares;
+            this.price = price;
+            this.date = date;
+        }
+    }
+}
